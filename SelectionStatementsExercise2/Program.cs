e4ebec93da38d2223ae51195b2a3b446364b936b
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-05-16 09:48:39 -0400

    Created school subject switch statements

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); // Evaluate to a string
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+
+                case "History":
+                    Console.WriteLine("History is a very useful subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a very useful subject!");
+                    break;
+                case "PhysEd":
+                    Console.WriteLine("PhysEd is a fun subject!");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is one of the harder subjects to learn!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I havn't taken {subject} before.");
+                    break;
+
+
+            }
         }
     }
 }
